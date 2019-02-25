0e18f688852f7aee3d3862117c75f8789e7cb934
mikule1 <5632Openbsd>
2019-02-26 00:40:17 +0200

    First commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import 'bootstrap';
+import jQuery from 'jquery';
+import  'popper.js';
+import  './assets/app.scss';
+
+window.$ = window.jQuery = jQuery;
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
